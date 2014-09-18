471500074be6dc832ff9e27277ea050f907b2727
Joshua Sumali <j.sumali@gmail.com>
2014-09-18 19:45:49 -0400

    remove java_skeleton test directory

--- test/java_skeleton/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns java-skeleton.core-test
-  (:require [clojure.test :refer :all]
-            [java-skeleton.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
